d2d89690384e72f3e63d31a50f9493c6c2749623
Brothin Das <78947331+Brothin@users.noreply.github.com>
2024-03-07 22:33:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ import mongoose from "mongoose";
 import parkingRoutes from "./routes/parkingRoutes.js";
 import dotenv from "dotenv";
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 
-// Connect to MongoDB using environment variable
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,10 +16,8 @@ mongoose.connect(process.env.MONGODB_URI, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-// Use routes
 app.use(parkingRoutes);
 
-// Start server using PORT from environment variable
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
